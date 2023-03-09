9dc89d5ed7d6f12bc27f327c9d38d405402f1974
topthinking <topthinking@qq.com>
2023-03-09 19:24:35 +0800

    fix:图片地址

--- plugins/publish.ts
+++ plugins/publish.ts
@@ -56,7 +56,7 @@ export default function compressServerCode() {
         path.join(__dirname, '..', 'lib', 'readme.md'),
         `# Chumi · [![NPM version](https://img.shields.io/npm/v/chumi.svg)](https://www.npmjs.com/package/chumi)  · [DOCUMENTATION](https://juejin.cn/post/7208099384071192635)
 
-![image.png](https://s1.ax1x.com/2023/03/07/ppeFpUP.png)
+![image.png](https://s1.ax1x.com/2023/03/09/ppnJJeA.png)
 
 ## Apis
 
